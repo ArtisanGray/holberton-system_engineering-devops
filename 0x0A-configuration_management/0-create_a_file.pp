eaa4b49481e6fc6b39bf697615a560d6084fc0e3
ArtisanGray <1697@holbertonschool.com>
2021-08-13 21:03:01 +0000

    fixed typo on 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 # creates a file with content in tmp folder
 file { '/tmp/holberton':
   ensure  => 'file',
-  mode    => '0774',
+  mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
   content => 'I love Puppet',
