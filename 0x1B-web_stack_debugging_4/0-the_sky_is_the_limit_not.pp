aae661c2e2b5901d80410d9cb6027d3ba637372f
ArtisanGray <1697@holbertonschool.com>
2021-10-07 23:38:10 +0000

    an attempt at fixing linter problems

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -3,8 +3,8 @@ file { '/etc/nginx/nginx.conf' :
   ensure => present,
 }
 exec { 'update worker_processes' :
-  command  => 'cat /etc/nginx/nginx.conf | sed \'s/worker_processes\ \
-4/worker_processes 1024/g\' | sudo tee /etc/nginx/nginx.conf ;\
-service nginx restart',
+  command  => 'cat /etc/nginx/nginx.conf |\
+  sed \'s/worker_processes 4/worker_processes 1024/g\' |\
+  sudo tee /etc/nginx/nginx.conf ; service nginx restart',
   provider => 'shell',
 }
